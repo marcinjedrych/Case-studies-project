613522bc67c0819c97914c31cfe18710069dedcc
rietvergauwe <130758045+rietvergauwe@users.noreply.github.com>
2023-06-10 17:52:21 +0200

    Add files via upload

---
+++ SPM/SecondLevelAnalysisWrist.m
@@ -0,0 +1,87 @@
+%-----------------------------------------------------------------------
+% Job saved on 07-Jun-2023 10:19:10 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = {'D:\Riet\CaseStudies\Analyses\SecondLevel\Wrist'};
+matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = {
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-01\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-02\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-03\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-05\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-06\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-07\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-08\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-09\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-10\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-11\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-12\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-13\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-14\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-16\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-17\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-18\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-20\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-21\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-22\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-24\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-25\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-26\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-29\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-30\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-31\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-32\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-33\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-34\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-35\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-36\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-37\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-38\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-39\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-42\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-43\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-44\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-45\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-46\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-47\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-48\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-49\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-50\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-51\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-52\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-53\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-54\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-55\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-56\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-57\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-58\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-59\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-60\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-61\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-62\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-63\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-65\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-66\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-67\con_0002.nii,1'
+                                                          'D:\Riet\CaseStudies\Analyses\FirstLevel\sub-68\con_0002.nii,1'
+                                                          };
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+
+%% Estimation
+matlabbatch{2}.spm.stats.fmri_est.spmmat = {'D:\Riet\CaseStudies\Analyses\SecondLevel\Wrist\SPM.mat'};
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+
+%% Contrasts
+matlabbatch{3}.spm.stats.con.spmmat = {'D:\Riet\CaseStudies\Analyses\SecondLevel\Wrist\SPM.mat'};
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'Wrist-Rest';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = 1;
+
+spm_jobman('run', matlabbatch);
