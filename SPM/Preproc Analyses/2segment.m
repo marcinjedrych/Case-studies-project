0ccaf977a50745a239979f31d340cc287f1f7234
rietvergauwe <riet.vergauwe@ugent.be>
2023-06-10 14:54:40 +0200

    Reordening of folders

---
+++ SPM/Preproc Analyses/2segment.m
@@ -0,0 +1,54 @@
+%-----------------------------------------------------------------------
+% Job saved on 06-Jun-2023 20:21:06 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+
+%subjects = [01 02  03 04 05 06 07 08 09 10 11 12 13 14 16 17 18 20 21 22 23 24 25 26 27 29 30 31 32 33 34 35 36 37 38 39 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 65 66 67 68];
+subjects = [02 03];
+
+for subject=subjects
+    subject = num2str(subject, '%02d');
+
+matlabbatch{1}.spm.spatial.preproc.channel.vols = {['D:\Riet\CaseStudies\Analyses\Data\anat\sub-' subject '\ses-1\anat\rsub-' subject '_ses-1_run-01_T1w.nii,1']};
+matlabbatch{1}.spm.spatial.preproc.channel.biasreg = 0.001;
+matlabbatch{1}.spm.spatial.preproc.channel.biasfwhm = 60;
+matlabbatch{1}.spm.spatial.preproc.channel.write = [0 1];
+matlabbatch{1}.spm.spatial.preproc.tissue(1).tpm = {'C:\Users\verga\OneDrive - UGent\School\Psychologie - experimentele\3e master\4 Case Studies in the Analysis of Experimental Data\Analyses\MATLAB\spm12\spm12\tpm\TPM.nii,1'};
+matlabbatch{1}.spm.spatial.preproc.tissue(1).ngaus = 1;
+matlabbatch{1}.spm.spatial.preproc.tissue(1).native = [1 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(1).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = {'C:\Users\verga\OneDrive - UGent\School\Psychologie - experimentele\3e master\4 Case Studies in the Analysis of Experimental Data\Analyses\MATLAB\spm12\spm12\tpm\TPM.nii,2'};
+matlabbatch{1}.spm.spatial.preproc.tissue(2).ngaus = 1;
+matlabbatch{1}.spm.spatial.preproc.tissue(2).native = [1 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(2).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = {'C:\Users\verga\OneDrive - UGent\School\Psychologie - experimentele\3e master\4 Case Studies in the Analysis of Experimental Data\Analyses\MATLAB\spm12\spm12\tpm\TPM.nii,3'};
+matlabbatch{1}.spm.spatial.preproc.tissue(3).ngaus = 2;
+matlabbatch{1}.spm.spatial.preproc.tissue(3).native = [1 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(3).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = {'C:\Users\verga\OneDrive - UGent\School\Psychologie - experimentele\3e master\4 Case Studies in the Analysis of Experimental Data\Analyses\MATLAB\spm12\spm12\tpm\TPM.nii,4'};
+matlabbatch{1}.spm.spatial.preproc.tissue(4).ngaus = 3;
+matlabbatch{1}.spm.spatial.preproc.tissue(4).native = [1 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(4).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = {'C:\Users\verga\OneDrive - UGent\School\Psychologie - experimentele\3e master\4 Case Studies in the Analysis of Experimental Data\Analyses\MATLAB\spm12\spm12\tpm\TPM.nii,5'};
+matlabbatch{1}.spm.spatial.preproc.tissue(5).ngaus = 4;
+matlabbatch{1}.spm.spatial.preproc.tissue(5).native = [1 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(5).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = {'C:\Users\verga\OneDrive - UGent\School\Psychologie - experimentele\3e master\4 Case Studies in the Analysis of Experimental Data\Analyses\MATLAB\spm12\spm12\tpm\TPM.nii,6'};
+matlabbatch{1}.spm.spatial.preproc.tissue(6).ngaus = 2;
+matlabbatch{1}.spm.spatial.preproc.tissue(6).native = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(6).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.warp.mrf = 1;
+matlabbatch{1}.spm.spatial.preproc.warp.cleanup = 1;
+matlabbatch{1}.spm.spatial.preproc.warp.reg = [0 0.001 0.5 0.05 0.2];
+matlabbatch{1}.spm.spatial.preproc.warp.affreg = 'mni';
+matlabbatch{1}.spm.spatial.preproc.warp.fwhm = 0;
+matlabbatch{1}.spm.spatial.preproc.warp.samp = 3;
+matlabbatch{1}.spm.spatial.preproc.warp.write = [0 1];
+matlabbatch{1}.spm.spatial.preproc.warp.vox = NaN;
+matlabbatch{1}.spm.spatial.preproc.warp.bb = [NaN NaN NaN
+                                              NaN NaN NaN];
+
+spm_jobman('run', matlabbatch);
+
+end % end subject loop
